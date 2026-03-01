5192bfa9aa07d70823b75439390e4a0ca4caf8c6
semonhany <semonhany848@gmail.com>
2026-03-02 00:45:31 +0200

    add authentication models content

---
+++ backend/models/auth-temps/otp.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+
+const OTPSchema= new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        index: true,
+        required: true,
+    },
+
+    for:{
+        type:String,
+        enum: ["verifyPhone", "verifyEmail"],
+        required: true,
+        default: null,
+        index: true,
+    },
+
+    otpCode:{
+        type: String,
+        minlength: 6,
+        maxlength:6,
+        index: true,
+        required: true,
+    },
+
+    isVerfied:{
+        type: Boolean,
+        default: false,
+    },
+
+    otpExpiry:{
+        type: Date,
+        index: true,
+        required: true,
+        default: ()=> newDate(Date.now()+ 10+60+1000), //10 min
+    },
+
+    otpAttempts:{
+        type: Number,
+        min:0,
+        max: 3, //per hour
+        default:0,
+        index: true,
+    },
+
+    firstAttemptAt: Date,
+
+}, {timestamps: true, versionKey: false});
+
+export const otpModel=mongoose.model("otp", OTPSchema);
--- backend/models/users/deliveryWorker.js
+++ backend/models/users/deliveryWorker.js
@@ -1,8 +1,38 @@
 import userModel from './user.js';
 
 const DeliveryWorkerSchema= new mongoose.Schema({
-
-})
+    vehicleType:{
+        type: String,
+        enum: ["motorcycle", "car", "public_transit", "other"],
+        required: true,
+        index:1,
+    },
+    licensePlate:{
+        type: String,
+        default: null,
+        index:1,
+    },
+    rating:{
+        type: Number,
+        min:0,
+        max:5,
+        default:0,
+    },
+    totalOrders:{
+        number: Number,
+        mi:0,
+        default:0,
+        index: true,
+    },
+    deliveredOrders:{
+        type:[{type:mongoose.Schema.Types.ObjectId, ref: 'order', index: true}],
+    },
+    currentDelivery:{type:mongoose.Schema.Types.ObjectId, ref: 'order',},
+    isAvailable:{
+        type: Boolean,
+        default: false,
+    }
+});
 
 const deliveryWorkerModel= userModel.discriminator("delivery_worker", DeliveryWorkerSchema);
 export {userModel, deliveryWorkerModel};
