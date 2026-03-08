00e92f9ce05076a52be4f5d80ec301dae35c2e3b
Eman Ahmed <emo216276@gmail.com>
2026-03-08 21:46:39 +0200

    Implement register and login controllers with role-based data handling

--- backend/controllers/auth/logout.js
+++ backend/controllers/auth/logout.js
@@ -1,7 +1,11 @@
 const userModel= require('../../models/users/user');
 
 const logoutController= async (req, res)=>{
+    try{
 
+    } catch(error){
+        res.status(500).json({ message: error.message})
+    }    
 }
 
 module.exports= logoutController;
