265624317641497e9874d9be8b936194049de8ce
ayaabduljawad750-cyber <ayaabduljawad750@gmail.com>
2026-03-08 23:41:42 +0200

    reset password

--- backend/models/auth-temps/otp.js
+++ backend/models/auth-temps/otp.js
@@ -1,52 +1,58 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
-const OTPSchema= new mongoose.Schema({
-    userId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-        index: true,
-        required: true,
+const OTPSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      index: true,
+      required: true,
     },
 
-    for:{
-        type:String,
-        enum: ["verifyPhone", "verifyEmail"],
-        required: true,
-        default: null,
-        index: true,
+    for: {
+      type: String,
+      enum: ["verifyPhone", "verifyEmail"],
+      required: true,
+      default: null,
+      index: true,
     },
 
-    otpCode:{
-        type: String,
-        minlength: 6,
-        maxlength:6,
-        index: true,
-        required: true,
+    otpCode: {
+      type: String,
+      minlength: 6,
+      maxlength: 6,
+      index: true,
+      required: true,
     },
 
-    isVerfied:{
-        type: Boolean,
-        default: false,
+    isVerfied: {
+      type: Boolean,
+      default: false,
     },
 
-    otpExpiry:{
-        type: Date,
-        index: true,
-        required: true,
-        default: ()=> newDate(Date.now()+ 10+60+1000), //10 min
+    isActive: {
+      type: Boolean,
     },
 
-    otpAttempts:{
-        type: Number,
-        min:0,
-        max: 3, //per hour
-        default:0,
-        index: true,
+    otpExpiry: {
+      type: Date,
+      index: true,
+      required: true,
+      default: Date.now() + 10 * 60 * 1000, //10 min
+    },
+
+    otpAttempts: {
+      type: Number,
+      min: 0,
+      max: 3, //per hour
+      default: 0,
+      index: true,
     },
 
     firstAttemptAt: Date,
+  },
+  { timestamps: true, versionKey: false },
+);
 
-}, {timestamps: true, versionKey: false});
-
-const otpModel= mongoose.model("otp", OTPSchema);
-module.exports= otpModel;
+const otpModel = mongoose.model("otp", OTPSchema);
+module.exports = otpModel;
