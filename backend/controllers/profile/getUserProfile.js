469160ad03d700d39b7814e821057e1b37d78904
semonhany <semonhany848@gmail.com>
2026-03-26 10:59:38 +0200

    edit getUserProfile and checkAuth middleware

--- backend/controllers/profile/getUserProfile.js
+++ backend/controllers/profile/getUserProfile.js
@@ -1,10 +1,13 @@
+
 const getUserProfileController= async (req, res)=>{
-    try{
+    try {
         const user= req.user;
 
-        res.status(200).json(user);
-    }catch(error){
-        res.status(500).json({message: "internal server error", error});
+        return res.status(200).json(user);
+    } catch (error) {
+        
+        return res.status(500).json({
+            message: "Error fetching user profile", error: error.message });
     }
 }
 
