4a31d916fdcc92717533b7f2db8f210f5e5ebae1
ayaabduljawad750-cyber <ayaabduljawad750@gmail.com>
2026-04-24 09:12:22 +0300

    Edit product by Id

--- backend/router/products.js
+++ backend/router/products.js
@@ -8,6 +8,8 @@ const checkAuth = require("../middleware/checkAuth");
 const deleteProductController = require("../controllers/products/deleteProduct.js");
 const checkRole = require("../middleware/checkRole.js");
 
+const editProductById = require("../controllers/products/editProductById");
+
 const router = express.Router();
 
 router.use(checkAuth(true));
@@ -16,8 +18,10 @@ router.get("/:id", getProductById);
 router.use(checkAuth());
 
 // router.use(checkRole("store_owner"));
-router.post("/", upload.array("images", 7), addNewProductController,);
+router.post("/", upload.array("images", 7), addNewProductController);
 router.delete("/:id", deleteProductController);
+
+router.put("/:id", upload.array("images", 7),editProductById)
 
 // router.use(checkRole("client"));
 router.post("/:id/rating", rateProductController);
