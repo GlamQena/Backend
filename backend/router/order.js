f48701b457a2005bf332afddea05779c6e4e6c5f
ayaabduljawad750-cyber <ayaabduljawad750@gmail.com>
2026-04-24 11:38:54 +0300

    getOrdersByOwnerStoreId

--- backend/router/order.js
+++ backend/router/order.js
@@ -6,7 +6,7 @@ const placeOrderController = require("../controllers/order/placeOrder");
 const setOrderStatusController = require("../controllers/order/setOrderStatus");
 const checkAuth = require("../middleware/checkAuth");
 const checkRole = require("../middleware/checkRole");
-
+const getOrdersByOwnerStoreId = require("../controllers/order/getOrdersByOwnerStoreId")
 const router = express.Router();
 
  //no checkAuth middleware to allow for paymob http requests 
@@ -17,6 +17,7 @@ router.use(checkAuth());
 
 router.post("/", placeOrderController);
 router.get("/history", getOrderHistoryController);
+router.get("/",getOrdersByOwnerStoreId) // for owner store
 router.post("/:id/payment", paymentCheckoutController);
 router.patch("/:id/status", setOrderStatusController);
 
