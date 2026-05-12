b7302c9662923f1630332bee1b953be20f5aa36b
Abeer Atef <abeer.atef.mohammed@gmail.com>
2026-05-12 23:01:55 +0300

    feat: add getActiveClients and implement getUserById

--- backend/router/users.js
+++ backend/router/users.js
@@ -1,15 +1,20 @@
-const express= require("express");
+const express = require("express");
 const addToWishlist = require("../controllers/users/addToWishlist");
 const removeFromWishlist = require("../controllers/users/removeFromWishlist");
 const checkAuth = require("../middleware/checkAuth");
 const checkRole = require("../middleware/checkRole");
-
-const router= express.Router();
+const getActiveClients = require("../controllers/users/getActiveClients");
+const getUserById = require("../controllers/users/getUserById");
+const router = express.Router();
 
 router.use(checkAuth());
-router.use(checkRole("client"));
+// router.use(checkRole("client"));
 
-router.post("/me/wishlist", addToWishlist);
-router.delete("/me/wishlist", removeFromWishlist);
+router.post("/me/wishlist", checkRole("client"), addToWishlist);
+router.delete("/me/wishlist", checkRole("client"), removeFromWishlist);
 
-module.exports= router;
+router.get("/active-clients", checkRole("store_owner"), getActiveClients);
+
+router.get("/:id", checkRole("admin"), getUserById);
+
+module.exports = router;
