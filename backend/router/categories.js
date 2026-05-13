6c5284f2ee4bab05884f4fb48b028c9e0108b0ea
ayaabduljawad750-cyber <ayaabduljawad750@gmail.com>
2026-05-13 11:47:55 +0300

    edit and delete category

--- backend/router/categories.js
+++ backend/router/categories.js
@@ -3,6 +3,8 @@ const checkAuth= require("../middleware/checkAuth");
 const getCategoriesController= require("../controllers/categories/getCategories");
 const addCategoryController= require("../controllers/categories/addCategory");
 const checkRole = require("../middleware/checkRole");
+const editCategoryById = require("../controllers/categories/editCategory");
+const deleteCategoryById = require("../controllers/categories/deleteCategory");
 
 const router= express.Router();
 
@@ -10,5 +12,6 @@ router.use(checkAuth());
 router.get("/", getCategoriesController);
 
 router.post("/", checkRole("admin"), addCategoryController);
-
+router.put("/:id",checkRole("admin"),editCategoryById)
+router.delete("/:id",checkRole("admin"),deleteCategoryById)
 module.exports= router;
