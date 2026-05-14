acbfd1bbe29136382632dc437f325eb7419122fb
Abeer Atef <abeer.atef.mohammed@gmail.com>
2026-05-14 22:21:35 +0300

    feat: add getUsers , getAllOrders , rejectRequest

--- backend/router/users.js
+++ backend/router/users.js
@@ -5,10 +5,13 @@ const checkAuth = require("../middleware/checkAuth");
 const checkRole = require("../middleware/checkRole");
 const getActiveClients = require("../controllers/users/getActiveClients");
 const getUserById = require("../controllers/users/getUserById");
+const rejectRequest = require("../controllers/admin/rejectRequest");
+const getUsers = require("../controllers/users/getUsers");
 const router = express.Router();
 
-router.use(checkAuth());
+ router.use(checkAuth());
 // router.use(checkRole("client"));
+router.get("/",getUsers);
 
 router.post("/me/wishlist", checkRole("client"), addToWishlist);
 router.delete("/me/wishlist", checkRole("client"), removeFromWishlist);
@@ -16,5 +19,8 @@ router.delete("/me/wishlist", checkRole("client"), removeFromWishlist);
 router.get("/active-clients", checkRole("store_owner"), getActiveClients);
 
 router.get("/:id", checkRole("admin"), getUserById);
+
+
+router.patch("/reject-request/:id", checkRole("admin"), rejectRequest);
 
 module.exports = router;
