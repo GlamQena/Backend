dddeea1ab499804f18287ca3eff3ac05b91a410a
ayaabduljawad750-cyber <ayaabduljawad750@gmail.com>
2026-05-15 15:20:13 +0300

    test add user

--- backend/router/users.js
+++ backend/router/users.js
@@ -3,13 +3,15 @@ const addToWishlist = require("../controllers/users/addToWishlist");
 const removeFromWishlist = require("../controllers/users/removeFromWishlist");
 const checkAuth = require("../middleware/checkAuth");
 const checkRole = require("../middleware/checkRole");
+const addUser = require("../controllers/users/addUser");
 
 const router= express.Router();
 
 router.use(checkAuth());
-router.use(checkRole("client"));
 
-router.post("/me/wishlist", addToWishlist);
-router.delete("/me/wishlist", removeFromWishlist);
+router.post("/me/wishlist", checkRole("client"),addToWishlist);
+router.delete("/me/wishlist", checkRole("client"),removeFromWishlist);
+
+router.post("/addUser",checkRole(["admin"]),addUser)
 
 module.exports= router;
