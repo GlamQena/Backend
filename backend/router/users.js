934ccec896711b777113ed5212f38019730fa34b
ayaabduljawad750-cyber <ayaabduljawad750@gmail.com>
2026-05-15 21:41:42 +0300

    test delte user

--- backend/router/users.js
+++ backend/router/users.js
@@ -8,6 +8,9 @@ const getUserById = require("../controllers/users/getUserById");
 const rejectRequest = require("../controllers/admin/rejectRequest");
 const getUsers = require("../controllers/users/getUsers");
 const addUser = require("../controllers/users/addUser");
+const deleteUserController = require("../controllers/users/deleteUser");
+const approveRequestDeletion = require("../controllers/admin/approveRequestDeletion");
+const pendingRequest = require("../controllers/admin/pendingRequest");
 const router = express.Router();
 
  router.use(checkAuth());
@@ -22,7 +25,10 @@ router.get("/active-clients", checkRole("store_owner"), getActiveClients);
 router.get("/:id", checkRole("admin"), getUserById);
 
 router.post("/addUser",checkRole("admin"),addUser)
+router.delete("/:id",checkRole("admin"),deleteUserController)
 
 router.patch("/reject-request/:id", checkRole("admin"), rejectRequest);
+router.patch("/approve-request/:id",checkRole("admin"),approveRequestDeletion);// after approve go to delete end point (click delete button in frontend)
+router.patch("/pending-request/:id",checkRole("admin"),pendingRequest) // if admin change his opinion after approve (click cancel button in frontend instead of delete button) return to pending 
 
 module.exports = router;
